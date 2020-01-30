427746166f5ff0c10c212f5215a40d5af3919968
El vostre nom <usuari@domini.com>
2020-01-30 14:24:31 +0100

    post login

--- portal/views.py
+++ portal/views.py
@@ -6,6 +6,13 @@ import requests
 def login(request):
   if request.method == 'GET':
 	  return render(request, 'login.html')
+  if request.method == 'POST':
+    nom = request.POST.get('fname', '')
+    cognom = request.POST.get('lname', '')
+    lloc = request.POST.get('lloc', '')
+    edat = request.POST.get('edat', '')
+    
+    return render(request, 'index.html')
 
 def home(request):
     if request.method == 'GET':
