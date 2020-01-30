478dfa9550a1c922e3c2820e1da03abb844fee1e
El vostre nom <usuari@domini.com>
2020-01-30 17:17:25 +0100

    hj

--- inite/settings.py
+++ inite/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'inite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db_dks',
         'USER': 'u_dks',
         'PASSWORD': 'NTExMmZhMmU3',
