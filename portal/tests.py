0baced20399b4d41bca119f1f67c83e6b1336043
xXtz <xXtz@xXtz.xXtz>
2020-02-01 08:46:30 -0500

    fix post in loggin and start tests

--- portal/tests.py
+++ portal/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+import portal.views as my_view
 
 # Create your tests here.
+
+class loginViewTest(TestCase):
+
+  def setUP(self):
+    self.factory = RequestFactory()
+
+  def test_view_login_returns_200_if_not_logged():
+    response = self.client.get('/loggin/'
+
+  def test_view_login_redirects_if_logged():
